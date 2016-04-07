9c980692763e2e35c36ad4d7b4c55dc16b59802a
Diana Siteva <dianasiteva@hotmail.com>
2016-04-07 15:28:12 +0300

    Модул 06, Водео 02 - създаване на dll файлове

---
+++ MP01/UserInput/UserInput.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UserInput
+{
+	public class UInput
+	{
+		public UInput ()
+		{
+		}
+
+		public void justTesting()     //примерна функция
+		{
+			Console.WriteLine ( "Стартиране на UserInput.UInput.justTesting ()" );
+		}
+	}
+}
+
