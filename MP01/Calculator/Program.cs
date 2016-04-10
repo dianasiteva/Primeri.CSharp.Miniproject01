5b2eccca2f74b793b78a71f053ad2aabebb213ea
Diana Siteva <dianasiteva@hotmail.com>
2016-04-10 16:50:19 +0300

    Модул 06, Видео 03 - архитектура на програма

--- MP01/Calculator/Program.cs
+++ MP01/Calculator/Program.cs
@@ -5,15 +5,14 @@ namespace Calculator
 	class MainClass
 	{
 		//Дефиниране на библиотеки
-		public UserInput.UInput uInput=new UserInput.UInput();
+		public UserInput.UInput uInput=new UserInput.UInput();  //не е static
 
 
 
 
-		public static void Main (string[] args)
+		public static void Main (string[] args)              //static
 		{
-			Console.WriteLine ("Hello World!");
-			MainClass _program = new MainClass ();
+			MainClass _program = new MainClass ();          //предефинира се за заделяне на памет, защото е static,  а вика не static
 			_program.uInput.justTesting ();
 
 		}
